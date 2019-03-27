06f195562337b98171073a123560c28b11d65cb4
visualstudio <visualstudio@122-PC07>
2019-03-27 10:23:07 -0400

    committ 10:20

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Downloading file");
+            Download();
+            Console.ReadLine();
         }
+
+        static void Download()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Downloading complete");
+            });
+
+        }
+
     }
 }
